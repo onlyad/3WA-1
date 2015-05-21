2e3fc078036e3ad0819a7c8def0cb2eb29cdac59
onlyad9 <zezenn@gmail.com>
2015-05-21 18:22:28 -0400

    Update JS02_06.js

--- JS/JS02_06.js
+++ JS/JS02_06.js
@@ -28,11 +28,21 @@ for (var iCounter = 1; iCounter <= iVAL_MAX; iCounter += 2) {
 // Bonus
 var iValue;
 
-for (var iOddNumber = 0; iOddNumber <= 999; iOddNumber += 2) {
+for (var iOddNumber = 1; iOddNumber <= iVAL_MAX; iOddNumber += 2) {
     iValue = 0;
-
-
-
+    if( iOddNumber /5 == parseInt(iOddNumber / 5)) {
+        iValue = -iOddNumber;
+    }
+    else if (iOddNumber < 100) {
+        iValue = iOddNumber * iOddNumber;
+    }
+    else if (iOddNumber > 300){
+        iValue = iOddNumber;
+    }
+    if (iValue != 0) {
+        document.write("<br>Pour le chiffre: " + iOddNumber +  "La Valeur est: " + iValue);
+    }    
+    
 }
 
 
